8a095818e726c23f765c4cf6da5495585373d307
sena <mskucukk@outlook.com>
2021-04-12 22:01:54 +0300

    Alan Hesaplama

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        String islemler="İşlemler...\n"
+                       +"1.Kare Alan Hesapla\n"
+                       +"2.Üçgen Alan Hesapla\n"
+                       +"3.Daire Alan Hesapla\n"
+                       +"Çıkış : q";
+    }
+}
