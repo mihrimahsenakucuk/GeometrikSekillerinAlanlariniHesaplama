e7697aa0b40a709ca348c401773d058ac914b2e3
mihrimahsenakucuk <73707051+mihrimahsenakucuk@users.noreply.github.com>
2021-04-13 23:59:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,38 @@ public class Main {
                        +"2.Üçgen Alan Hesapla\n"
                        +"3.Daire Alan Hesapla\n"
                        +"Çıkış : q";
+        
+        
+        while(true){
+            System.out.println(islemler);
+            System.out.println("Hangi şeklin alanını hesaplamak istiyorsunuz);
+            String sekil_turu= scanner.nextLine();
+                               
+            if(sekil_turu.equals("q")){
+                System.out.println("Çıkış yapılıyor...);
+                break;
+          }
+           else if(sekil_turu.equals("1"){
+               System.out.println("Karenin kenarı: );
+               int kenar= scanner.nextInt();  
+               scanner.nextLine();                   
+               
+                sekil= new Kare("Kare1", kenar);
+                sekil.alanHesapla();
+           }
+                                  
+           else if(sekil_turu.equals("2"){
+               System.out.println("Üçgenin kenarı: );
+               ;                   
+                              
+
+                                  
+               
+               
+                sekil= new ucgen("Üçgen1", kenar);
+                sekil.alanHesapla();
+           }                   
+                   
+        }
     }
 }
